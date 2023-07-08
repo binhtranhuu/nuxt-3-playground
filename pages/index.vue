55d1683d0cabdeba204f8961b47f3e0e2d465ea6
binhth-1206 <tran.huu.binh@sun-asterisk.com>
2023-07-08 18:54:40 +0700

    feat: initial project

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container mx-auto bg-gray-50 w-full h-screen">
+    <div class="w-full h-full flex justify-center items-center">
+      <div class="max-w-md bg-white p-4 rounded-lg shadow">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro at illum
+        totam reprehenderit dolorem corrupti sapiente nulla maxime dicta, minus
+        nisi laudantium esse incidunt temporibus non molestias sed labore
+        eligendi!
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
