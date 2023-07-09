03afd014754f593ebb56d791ea495ea859c3ff56
binhth-1206 <tran.huu.binh@sun-asterisk.com>
2023-07-09 16:11:45 +0700

    feat: implement config extend

---
+++ pages/config/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>
+      appConfig:
+      <pre>{{ JSON.stringify(appConfig, null, 2) }}</pre>
+    </div>
+    <div>
+      runtimeConfig:
+      <pre>{{ JSON.stringify(runtimeConfig, null, 2) }}</pre>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const appConfig = useAppConfig();
+const runtimeConfig = useRuntimeConfig();
+</script>
