53cd32932a75b6590dc95e2142ed81e8ac8cc2dc
binhth-1206 <tran.huu.binh@sun-asterisk.com>
2023-07-09 23:10:44 +0700

    feat: implement i18n

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="flex items-center justify-center gap-2">
-    <NuxtLink to="/posts" class="max-w-md bg-white px-4 py-2 rounded-lg shadow">
+    <NuxtLink
+      :to="localePath('/posts')"
+      class="max-w-md bg-white px-4 py-2 rounded-lg shadow"
+    >
       Posts
     </NuxtLink>
-    <NuxtLink to="/users" class="max-w-md bg-white px-4 py-2 rounded-lg shadow">
+    <NuxtLink
+      :to="localePath('/users')"
+      class="max-w-md bg-white px-4 py-2 rounded-lg shadow"
+    >
       Users
     </NuxtLink>
   </div>
+
+  <div class="text-center">
+    <p class="text-base font-medium">{{ $t("name") }}</p>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+const localePath = useLocalePath();
+</script>
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="grid grid-cols-12 gap-3">
-      <div v-if="pending">Loading...</div>
-      <div v-if="error">Oop! Error...</div>
-      <div
-        v-for="post in posts"
-        :key="post.id"
-        class="col-span-3 bg-white p-2 rounded-lg shadow"
-      >
-        <h3 class="text-lg font-medium truncate">
-          <NuxtLink
-            :to="`/posts/${post.id}`"
-            class="hover:text-blue-600 hover:underline"
-          >
-            {{ post.title }}
-          </NuxtLink>
-        </h3>
-        <p class="text-base line-clamp-3">{{ post.body }}</p>
-      </div>
+    <div v-if="pending">Loading...</div>
+    <div v-if="error">Oop! Error...</div>
+    <div
+      v-for="post in posts"
+      :key="post.id"
+      class="col-span-3 bg-white p-2 rounded-lg shadow"
+    >
+      <h3 class="text-lg font-medium truncate">
+        <NuxtLink
+          :to="localePath(`/posts/${post.id}`)"
+          class="hover:text-blue-600 hover:underline"
+        >
+          {{ post.title }}
+        </NuxtLink>
+      </h3>
+      <p class="text-base line-clamp-3">{{ post.body }}</p>
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -27,6 +27,8 @@ useSeoMeta({
   title: "Posts screen",
   description: "This is the Posts screen",
 });
+
+const localePath = useLocalePath();
 
 const {
   data: posts,
